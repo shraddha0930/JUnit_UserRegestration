ec1776e45215b1b75fb0e7f7361487fea69a432d
shraddha0930 <shraddhaingole11@gmail.com>
2023-01-10 18:07:21 +0530

    lastname validation

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -10,4 +10,11 @@ public class UserRegistrationTest {
        boolean fName = userRegistration.validateFirstName("Shraddha");
         Assert.assertTrue(fName);
     }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean lName = userRegistration.validateLastName("sarode");
+        Assert.assertTrue(lName);
+    }
 }
