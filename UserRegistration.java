4cbdf83b5683d2b08ebd83fc545f0c3979fa1b74
shraddha0930 <shraddhaingole11@gmail.com>
2023-01-10 18:04:54 +0530

    validate last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,10 +3,14 @@ package day20Practiceproblem;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2}$";
+    private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2}$";
 
     public boolean validateFirstName(String firstName) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-        return pattern.matches(FIRST_NAME_PATTERN, firstName);
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matches(NAME_PATTERN, firstName);
+    }
+    public boolean validateLastName(String lName) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matches(NAME_PATTERN,lName);
     }
 }
