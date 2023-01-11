9294438e4917819c4624a387301574c95c895c79
shraddha0930 <shraddhaingole11@gmail.com>
2023-01-11 22:00:19 +0530

    custom exception added

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,26 +8,39 @@ public class UserRegistration {
     private static final String PHONE_PATTERN = "^[1-9]{2}(\\s)[6-9]{1}[0-9]{9}$";
     private static final String PASSWORD_PATTERN ="^[A-Za-z0-9!@#$%^&*]{8,}$";
 
-    public boolean validateFirstName(String fname) {
-        Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matches(NAME_PATTERN,fname);
+    public boolean validateFirstName(String name) throws UserRegException {
+        Pattern p = Pattern.compile(name);
+        if(!Pattern.matches(NAME_PATTERN, name)){
+            throw new UserRegException("Invalid First Name..!!");
+        }
+        return true;
     }
-
-    public boolean validateLastName(String lName) {
-        Pattern pattern = Pattern.compile(NAME_PATTERN);
-        return pattern.matches(NAME_PATTERN,lName);
+    public boolean validateLastName(String name) throws UserRegException {
+        Pattern p = Pattern.compile(name);
+        if(!Pattern.matches(NAME_PATTERN, name)){
+            throw new UserRegException("Invalid Last Name..!!");
+        }
+        return true;
     }
-
-    public boolean validateEmail(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        return pattern.matches(EMAIL_PATTERN,email);
+    public boolean validateEmail(String email) throws UserRegException {
+        Pattern p =Pattern.compile(email);
+        if(!Pattern.matches(EMAIL_PATTERN, email)){
+            throw new UserRegException("Invalid Email..!!");
+        }
+        return true;
     }
-    public boolean validatePhoneNumber(String phNumber) {
-        Pattern pattern = Pattern.compile(PHONE_PATTERN);
-        return pattern.matches(PHONE_PATTERN, phNumber);
+    public boolean validatePhoneNumber(String phoneNum) throws UserRegException {
+        Pattern p = Pattern.compile(phoneNum);
+        if(!Pattern.matches(PHONE_PATTERN, phoneNum)) {
+            throw new UserRegException("Invalid Phone Number..!!");
+        }
+        return true;
     }
-    public boolean validatePassword(String password){
-        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-        return pattern.matches(PASSWORD_PATTERN, password);
+    public boolean validatePassword(String password) throws Exception {
+        Pattern p = Pattern.compile(password);
+        if(!Pattern.matches(PASSWORD_PATTERN, password)){
+            throw new UserRegException("Invalid Password..!!");
+        }
+        return true;
     }
 }
