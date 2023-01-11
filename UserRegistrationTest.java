53714c5f22835e1bd4f03699d702127599c2a7e2
shraddha0930 <shraddhaingole11@gmail.com>
2023-01-11 17:59:47 +0530

    junit test for happy and sad test

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -3,38 +3,71 @@ package day20Practiceproblem;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertTrue;
-
 public class UserRegistrationTest {
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+    public void givenFirstName_WhenProper_ShouldReturnHappy() {
         UserRegistration userRegistration = new UserRegistration();
-       boolean fName = userRegistration.validateFirstName("Shraddha");
-        Assert.assertTrue(fName);
+        String fName = userRegistration.validateFirstName("Shraddha");
+        Assert.assertEquals("Happy", fName);
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() {
+    public void givenFirstName_WhenNotProper_ShouldReturnSad() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean lName = userRegistration.validateLastName("sarode");
-        Assert.assertTrue(lName);
+        String fName = userRegistration.validateFirstName("sra");
+        Assert.assertEquals("SAD", fName);
     }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnHappy() {
+        UserRegistration userRegistration = new UserRegistration();
+        String lName = userRegistration.validateLastName("sarode");
+        Assert.assertEquals("Happy",lName);
+    }
+
+    @Test
+    public void givenLastName_WhenNotProper_ShouldReturnSad() {
+        UserRegistration userRegistration = new UserRegistration();
+        String lName = userRegistration.validateFirstName("sra");
+        Assert.assertEquals("SAD", lName);
+    }
+
     @Test
     public void givenEmail_WhenProper_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean email = userRegistration.validateEmail("abc.xyz@bl.co.in");
-        Assert.assertTrue(email);
+        String email = userRegistration.validateEmail("abc.xyz@bl.co.in");
+        Assert.assertEquals("Happy",email);
     }
+    @Test
+    public void givenEmail_WhenNotProper_ShouldReturnSad() {
+        UserRegistration userRegistration = new UserRegistration();
+        String email = userRegistration.validateFirstName(".abc@yahoo.com");
+        Assert.assertEquals("SAD", email);
+    }
+
     @Test
     public void givenPhoneNumber_WhenProperWithSpace_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean phoneNumber = userRegistration.validatePhoneNumber("91 7822503725");
-        Assert.assertTrue(phoneNumber);
+        String phoneNumber = userRegistration.validatePhoneNumber("91 7822503725");
+        Assert.assertEquals("Happy",phoneNumber);
     }
     @Test
-    public void givenPassword_WhenProper_ShouldReturnTrue(){
+    public void givenPhoneNumber_WhenNotProper_ShouldReturnSad() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean password = userRegistration.validatePassword("Shr346$174sh");
-        Assert.assertTrue(password);
+        String phoneNumber = userRegistration.validateFirstName("sra");
+        Assert.assertEquals("SAD", phoneNumber);
+    }
+
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        String password = userRegistration.validatePassword("Shr346$174sh");
+        Assert.assertEquals("Happy",password);
+    }
+    @Test
+    public void givenPassword_WhenNotProper_ShouldReturnSad() {
+        UserRegistration userRegistration = new UserRegistration();
+        String password = userRegistration.validateFirstName("sra");
+        Assert.assertEquals("SAD", password);
     }
 }
